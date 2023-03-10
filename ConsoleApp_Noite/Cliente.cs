ea2469489a1833661ee1b99809f02097e0b5ca1c
VancleyVieira <vancley.vieira@fatec.sp.gov.br>
2023-03-10 20:43:14 -0300

    Add project files.

---
+++ ConsoleApp_Noite/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Cliente
+    {
+        private string nome;
+
+        public Cliente(string nome) {
+            this.nome = nome;
+        }
+
+    }
+}
