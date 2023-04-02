48d020026a17d272511c866f662c7e2afe30a4d1
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-04-02 09:10:44 -0300

    Criacao das classes e exercicios pedidos

---
+++ ConsoleApp_Noite/Heranca/Veiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite.Heranca
+{
+    public class Veiculo
+    {
+        public int numeroAssentos;
+        public int numeroPortas;
+        public bool temMotor;
+
+        public string ExibirDados()
+        {
+            if (temMotor == true)
+            {
+                return "Com motor, Assentos:  " + numeroAssentos + " Nº Portas: " + numeroPortas;
+            }
+            else
+            {
+                return "Assentos: " + numeroAssentos + " Nº Portas:" + numeroPortas;
+            }
+
+        }
+    }
+}
--- ConsoleApp_Noite/Program.cs
+++ ConsoleApp_Noite/Program.cs
@@ -1,2 +1,141 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_Noite;
+using ConsoleApp_Noite.Heranca;
+
 Console.WriteLine("Hello, World!");
+
+Produto p1 = new Produto(1, 10.90M, "Cervejinha");
+
+Console.WriteLine(p1.getDescricao() + " " + p1.getId());
+
+
+p1.setValor(11.85M);
+p1.setDescricao("Cerveja Skol 1 litrão");
+
+Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
+
+Endereco endereco = new Endereco(
+    "Rua Mario Miziara",
+    "91",
+    "Sobral",
+    "15906304",
+    "Taquaritinga",
+    "SP");
+
+Cliente c1 = new Cliente(1, "Gabriele", "991340447", endereco);
+Cliente c2 = new Cliente(2, "Gabriela", "991340447", endereco);
+Cliente c3 = new Cliente(3, "Ryan", "991340447", endereco);
+Cliente c4 = new Cliente(4, "Fernanda", "991340447", endereco);
+Cliente c5 = new Cliente(5, "Giovanni", "991340447", endereco);
+
+Console.WriteLine(c2.getEndereco().getRua() + " " + c2.getEndereco().getNumero());
+
+c2.getEndereco().setNumero("135");
+
+Console.WriteLine(c2.getEndereco().getNumero());
+Console.WriteLine(c2.getEndereco().EnderecoCompleto());
+
+c2.getEndereco().EnderecoCompleto();
+
+Carro gol = new Carro();
+gol.temMotor = true;
+gol.numeroPortas = 4;
+gol.numeroAssentos = 5;
+
+Bike caloi = new Bike();
+caloi.temMotor = false;
+caloi.numeroPortas = 0;
+caloi.numeroAssentos = 1;
+
+gol.fabricante = "VW";
+gol.cintoSeguranca = true;
+gol.arCondicionado = false;
+
+Carro Saveiro = new Carro();
+Carro Palio = new Carro();
+Carro Uno = new Carro();
+
+Saveiro.fabricante = "VW";
+Palio.fabricante = "Fiat";
+Uno.fabricante = "Fiat";
+
+Bike speed = new Bike();
+speed.temMotor = false;
+speed.numeroPortas = 0;
+speed.numeroAssentos = 1;
+
+// Faça uma ação chamada ExibirDados e
+// mostre as informações na tela pelo Console.Writeline
+Console.WriteLine(speed.ExibirDados());
+Console.WriteLine(gol.ExibirDados());
+
+
+
+List<Carro> listaCarros = new List<Carro>();
+listaCarros.Add(gol);
+listaCarros.Add(Palio);
+listaCarros.Add(Uno);
+listaCarros.Add(Saveiro);
+
+Console.WriteLine(listaCarros[0].fabricante);
+listaCarros.Remove(Palio);
+
+for (int i = 0; i < listaCarros.Count; i++)
+{
+    Console.WriteLine(listaCarros[i].ExibirDados());
+}
+
+foreach (var item in listaCarros)
+{
+    Console.WriteLine(item.ExibirMarca());
+}
+
+
+//Imprima na tela todos os clientes desse contexto;
+//Dica crie uma lista para eles.
+
+List<Cliente> listaClientes = new List<Cliente>();
+listaClientes.Add(c1);
+listaClientes.Add(c2);
+listaClientes.Add(c3);
+listaClientes.Add(c4);
+listaClientes.Add(c5);
+
+foreach (var cliente in listaClientes)
+{
+    Console.WriteLine("ID: " + cliente.getId() + ", Nome: " + cliente.getNome() + ", Telefone: " + cliente.getTelefone());
+    Console.WriteLine("Endereço: " + cliente.getEndereco().EnderecoCompleto());
+}
+
+
+//Crie uma lista com 560 produtos
+//Imprima na tela
+
+List<Produto> produtos = new List<Produto>();
+
+for (int i = 1; i <= 560; i++)
+{
+    Produto produto = new Produto(i, i * 2.5M, "Produto " + i);
+    produtos.Add(produto);
+}
+
+foreach (var produto in produtos)
+{
+    Console.WriteLine(produto.getDescricao());
+}
+
+//Crie uma lista que aceite qualquer tipo de veiculo
+//Imprimam na tela
+
+List<Veiculo> listaVeiculos = new List<Veiculo>();
+listaVeiculos.Add(gol);
+listaVeiculos.Add(Palio);
+listaVeiculos.Add(Uno);
+listaVeiculos.Add(Saveiro);
+listaVeiculos.Add(caloi);
+listaVeiculos.Add(speed);
+
+for (int i = 0; i < listaVeiculos.Count; i++)
+{
+    Console.WriteLine(listaVeiculos[i].ExibirDados());
+}
